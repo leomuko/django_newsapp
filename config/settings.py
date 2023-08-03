4a3dcf84ae95d7af0e9e275eb7325002eb9d4617
leomuko <kezzieleo@gmail.com>
2023-08-03 12:37:47 +0300

    Add Bootstrap and Django Crispy forms

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third Party
+    'crispy_forms',
+    # Local
     'accounts',
     'pages',
 ]
@@ -124,3 +127,4 @@ STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home' 
 LOGOUT_REDIRECT_URL = 'home' 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
